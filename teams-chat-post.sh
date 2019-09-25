cc856e0b413fbf5295374c369b1741073ec4a5d0
forrestlin <forrest_lin@htc.com>
2019-09-25 09:27:55 +0800

    updates

--- teams-chat-post.sh
+++ teams-chat-post.sh
@@ -1,201 +1,24 @@
 # env
 WEBHOOK_URL='https://outlook.office.com/webhook/7358ebd5-8e9e-4465-9f99-27d96fb2df38@afb5d3cf-2693-47e7-ade9-696a806ba95a/IncomingWebhook/405f6771329f427fb2418785c7b71440/b52bbfca-8b6f-4012-8ad9-dadf094c0961'
-SUMMARY='This Is A Summary'
-ACT_TITLE='This Is A Act Title'
-SUB_TITLE='This Is A Sub Title'
+TITLE='This Is A Title'
 COLOR='#000000'
 
 # formating
 MESSAGES_P1="$(echo "<strong> \` This is a message \` </strong>")"
 MESSAGES="$MESSAGES_P1 <br>"
-#JSON="{ \"@type\": \"MessageCard\", \"title\": \"${TITLE}\", \"themeColor\": \"${COLOR}\", \"text\": \"${MESSAGE}\" }"
-
-#cat << EOF > /tmp/tmp-message.json
-#{
-#  "@type": "MessageCard",
-#  "@context": "http://schema.org/extensions",
-#  "themeColor": "${COLOR}",
-#  "summary": "Larry Bryant created a new task",
-#  "sections": [
-#    {
-#      "activityTitle": "${ACT_TITLE}",
-#      "activitySubtitle": "On Project Tango",
-#      "activityImage": "https://teamsnodesample.azurewebsites.net/static/img/image5.png",
-#      "facts": [
-#        {
-#          "name": "Assigned to",
-#          "value": "Unassigned"
-#        },
-#        {
-#          "name": "Due date",
-#          "value": "Mon May 01 2017 17:07:18 GMT-0700 (Pacific Daylight Time)"
-#        },
-#        {
-#          "name": "Status",
-#          "value": "Not started"
-#        }
-#      ],
-#      "markdown": true
-#    }
-#  ],
-#  "potentialAction": [
-#    {
-#      "@type": "ActionCard",
-#      "name": "Add a comment",
-#      "inputs": [
-#        {
-#          "@type": "TextInput",
-#          "id": "comment",
-#          "isMultiline": false,
-#          "title": "Add a comment here for this task"
-#        }
-#      ],
-#      "actions": [
-#        {
-#          "@type": "HttpPOST",
-#          "name": "Add comment",
-#          "target": "http://..."
-#        }
-#      ]
-#    },
-#    {
-#      "@type": "ActionCard",
-#      "name": "Set due date",
-#      "inputs": [
-#        {
-#          "@type": "DateInput",
-#          "id": "dueDate",
-#          "title": "Enter a due date for this task"
-#        }
-#      ],
-#      "actions": [
-#        {
-#          "@type": "HttpPOST",
-#          "name": "Save",
-#          "target": "http://..."
-#        }
-#      ]
-#    },
-#    {
-#      "@type": "ActionCard",
-#      "name": "Change status",
-#      "inputs": [
-#        {
-#          "@type": "MultichoiceInput",
-#          "id": "list",
-#          "title": "Select a status",
-#          "isMultiSelect": "false",
-#          "choices": [
-#            {
-#              "display": "In Progress",
-#              "value": "1"
-#            },
-#            {
-#              "display": "Active",
-#              "value": "2"
-#            },
-#            {
-#              "display": "Closed",
-#              "value": "3"
-#            }
-#          ]
-#        }
-#      ],
-#      "actions": [
-#        {
-#          "@type": "HttpPOST",
-#          "name": "Save",
-#          "target": "http://..."
-#        }
-#      ]
-#    }
-#  ]
-#}
-#EOF
 
 cat << EOF > /tmp/tmp-message.json
 {
   "@type": "MessageCard",
-  "@context": "http://schema.org/extensions",
   "themeColor": "${COLOR}",
-  "summary": "${SUMMARY}",
-  "sections": [
-    {
-      "activityTitle": "${ACT_TITLE}",
-      "activitySubtitle": "${SUB_TITLE}",
-      "text": "${MESSAGES}",
-      "markdown": true
-    }
-  ],
+  "title": "${TITLE}",
+  "text": "${MESSAGES}",
   "potentialAction": [
     {
-      "@type": "ActionCard",
-      "name": "Add a comment",
-      "inputs": [
-        {
-          "@type": "TextInput",
-          "id": "comment",
-          "isMultiline": false,
-          "title": "Add a comment here for this task"
-        }
-      ],
-      "actions": [
-        {
-          "@type": "HttpPOST",
-          "name": "Add comment",
-          "target": "http://..."
-        }
-      ]
-    },
-    {
-      "@type": "ActionCard",
-      "name": "Set due date",
-      "inputs": [
-        {
-          "@type": "DateInput",
-          "id": "dueDate",
-          "title": "Enter a due date for this task"
-        }
-      ],
-      "actions": [
-        {
-          "@type": "HttpPOST",
-          "name": "Save",
-          "target": "http://..."
-        }
-      ]
-    },
-    {
-      "@type": "ActionCard",
-      "name": "Change status",
-      "inputs": [
-        {
-          "@type": "MultichoiceInput",
-          "id": "list",
-          "title": "Select a status",
-          "isMultiSelect": "false",
-          "choices": [
-            {
-              "display": "In Progress",
-              "value": "1"
-            },
-            {
-              "display": "Active",
-              "value": "2"
-            },
-            {
-              "display": "Closed",
-              "value": "3"
-            }
-          ]
-        }
-      ],
-      "actions": [
-        {
-          "@type": "HttpPOST",
-          "name": "Save",
-          "target": "http://..."
-        }
+      "@type": "OpenUri",
+      "name": "Ok Google",
+      "targets": [
+        { "os": "default", "uri": "https://www.google.com" }
       ]
     }
   ]
@@ -206,4 +29,129 @@ JSON=$(cat /tmp/tmp-message.json)
 
 # post to teams channel
 curl -H "Content-Type: application/json" -d "${JSON}" "${WEBHOOK_URL}"
+
+
+###############
+## exmaple 1 ##
+###############
+#
+#cat << EOF > /tmp/tmp-message.json
+#{
+#    "@type": "MessageCard",
+#    "@context": "http://schema.org/extensions",
+#    "themeColor": "0076D7",
+#    "summary": "Larry Bryant created a new task",
+#    "sections": [{
+#        "activityTitle": "![TestImage](https://47a92947.ngrok.io/Content/Images/default.png)Larry Bryant created a new task",
+#        "activitySubtitle": "On Project Tango",
+#        "activityImage": "https://teamsnodesample.azurewebsites.net/static/img/image5.png",
+#        "facts": [{
+#            "name": "Assigned to",
+#            "value": "Unassigned"
+#        }, {
+#            "name": "Due date",
+#            "value": "Mon May 01 2017 17:07:18 GMT-0700 (Pacific Daylight Time)"
+#        }, {
+#            "name": "Status",
+#            "value": "Not started"
+#        }],
+#        "markdown": true
+#    }],
+#    "potentialAction": [{
+#        "@type": "ActionCard",
+#        "name": "Add a comment",
+#        "inputs": [{
+#            "@type": "TextInput",
+#            "id": "comment",
+#            "isMultiline": false,
+#            "title": "Add a comment here for this task"
+#        }],
+#        "actions": [{
+#            "@type": "HttpPOST",
+#            "name": "Add comment",
+#            "target": "http://..."
+#        }]
+#    }, {
+#        "@type": "ActionCard",
+#        "name": "Set due date",
+#        "inputs": [{
+#            "@type": "DateInput",
+#            "id": "dueDate",
+#            "title": "Enter a due date for this task"
+#        }],
+#        "actions": [{
+#            "@type": "HttpPOST",
+#            "name": "Save",
+#            "target": "http://..."
+#        }]
+#    }, {
+#        "@type": "ActionCard",
+#        "name": "Change status",
+#        "inputs": [{
+#            "@type": "MultichoiceInput",
+#            "id": "list",
+#            "title": "Select a status",
+#            "isMultiSelect": "false",
+#            "choices": [{
+#                "display": "In Progress",
+#                "value": "1"
+#            }, {
+#                "display": "Active",
+#                "value": "2"
+#            }, {
+#                "display": "Closed",
+#                "value": "3"
+#            }]
+#        }],
+#        "actions": [{
+#            "@type": "HttpPOST",
+#            "name": "Save",
+#            "target": "http://..."
+#        }]
+#    }]
+#}
+#EOF
+
+###############
+## exmaple 2 ##
+###############
+#
+#cat << EOF > /tmp/tmp-message.json
+#{
+#  "@context": "https://schema.org/extensions",
+#  "@type": "MessageCard",
+#  "themeColor": "0072C6",
+#  "title": "Visit the Outlook Dev Portal",
+#  "text": "Click **Learn More** to learn more about Actionable Messages!",
+#  "potentialAction": [
+#    {
+#      "@type": "ActionCard",
+#      "name": "Send Feedback",
+#      "inputs": [
+#        {
+#          "@type": "TextInput",
+#          "id": "feedback",
+#          "isMultiline": true,
+#          "title": "Let us know what you think about Actionable Messages"
+#        }
+#      ],
+#      "actions": [
+#        {
+#          "@type": "HttpPOST",
+#          "name": "Send Feedback",
+#          "isPrimary": true,
+#          "target": "http://..."
+#        }
+#      ]
+#    },
+#    {
+#      "@type": "OpenUri",
+#      "name": "Learn More",
+#      "targets": [
+#        { "os": "default", "uri": "https://docs.microsoft.com/outlook/actionable-messages" }
+#      ]
+#    }
+#  ]
+#}
+#EOF
 
